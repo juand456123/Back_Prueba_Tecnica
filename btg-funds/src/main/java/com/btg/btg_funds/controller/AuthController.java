aa6422c75a88246a739f35e7456188bb9d5e88bc
Juan Guzman <151669226+juand456123@users.noreply.github.com>
2026-03-09 13:14:52 -0500

    probar pipeline de EXISTENTE

--- btg-funds/src/main/java/com/btg/btg_funds/controller/AuthController.java
+++ btg-funds/src/main/java/com/btg/btg_funds/controller/AuthController.java
@@ -28,7 +28,7 @@ public class AuthController {
     @PostMapping("/register") 
     @Operation(
         summary = "Registrar nuevo usuario",
-        description = "Permite registrar un nuevo usuario en el sistema"
+        description = "Permite registrar un nuevo usuario en el sistema PRUEBA EXISTENTE"
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Usuario registrado correctamente"),
